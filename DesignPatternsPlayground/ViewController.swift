b924bb70fab7ea86b08e8370b3a4c3bf6e1f09ef
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2015-12-17 13:54:08 +0700

    Initial commit

---
+++ DesignPatternsPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 12/17/15.
+//  Copyright © 2015 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
