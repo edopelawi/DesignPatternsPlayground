e6dffbec6e174cb846147440f27dd34b76aa2932
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2015-12-23 13:09:54 +0700

    Link creational patterns view controller in Main.storyboard to corresponding class

---
+++ DesignPatternsPlayground/ViewControllers/CreationalPatternsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreationalPatternsViewController.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 12/23/15.
+//  Copyright © 2015 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+class CreationalPatternsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBarHidden = false
+        title = "Creational Design Patterns"
+    }
+
+}
