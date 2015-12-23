3997bb4de9718365900d9e4ef9aa3f51e5ae5abe
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2015-12-23 15:03:54 +0700

    Initial commit for PatternType protocol

---
+++ DesignPatternsPlayground/Protocols/PatternType.swift
@@ -0,0 +1,15 @@
+//
+//  PatternType.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 12/23/15.
+//  Copyright © 2015 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+protocol PatternType {
+    
+    /// Returns total number of types for this PatternType.
+    static func numberOfTypes() -> Int
+}
