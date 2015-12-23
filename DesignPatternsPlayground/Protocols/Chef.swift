48e8cd42692beefad77c34af17f007ba9a81f75b
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2015-12-23 16:23:51 +0700

    Add Chef protocol

---
+++ DesignPatternsPlayground/Protocols/Chef.swift
@@ -0,0 +1,18 @@
+//
+//  Chef.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 12/23/15.
+//  Copyright © 2015 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+/**
+This class is created for Abstract Factory sample.
+*/
+protocol Chef {
+    
+    /// Returns `Food` that suits this `Chef`.
+    func cook() -> Food
+}
