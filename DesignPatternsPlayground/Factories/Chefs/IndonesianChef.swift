c6eea37092be171d4000843b2e7f1be8dbe71fe7
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2015-12-28 16:49:35 +0700

    Initial commit for IndonesianChef factory class

---
+++ DesignPatternsPlayground/Factories/Chefs/IndonesianChef.swift
@@ -0,0 +1,29 @@
+//
+//  IndonesianChef.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 12/28/15.
+//  Copyright © 2015 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+class IndonesianChef: Chef {
+    
+    let foodNames = [
+        "Ayam bakar", "Bubur ayam", "Empal gentong",
+        "Daun ubi tumbuk", "Gado-gado", "Gudeg",
+        "Iga penyet", "Karedok", "Ketoprak"
+    ]
+    
+    /// Returns `Indonesian` Food with random name.
+    func cook() -> Food {
+        var selectedFoodName = ""
+        
+        if let foodName = foodNames.randomElement() {
+            selectedFoodName = foodName
+        }
+        
+        return Food(type: .Indonesian, name: selectedFoodName)
+    }
+}
