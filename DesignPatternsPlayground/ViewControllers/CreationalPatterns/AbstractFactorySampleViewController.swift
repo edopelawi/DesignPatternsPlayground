c746989b458253ea9656a29fe91795e14022d731
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2015-12-28 17:44:38 +0700

    Initial commit for AbstractFactorySampleViewController

---
+++ DesignPatternsPlayground/ViewControllers/CreationalPatterns/AbstractFactorySampleViewController.swift
@@ -0,0 +1,56 @@
+//
+//  AbstractFactorySampleViewController.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 12/28/15.
+//  Copyright © 2015 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+class AbstractFactorySampleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private let cellIdentifier = "cellIdentifier"
+    
+    convenience init() {
+        self.init(nibName: "AbstractFactorySampleViewController", bundle: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+    
+    
+    @IBAction func randomizeFood(sender: AnyObject) {
+        tableView.reloadData()
+    }
+    
+    // MARK: - UITableView Data Source and Delegate -
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return FoodType.numberOfTypes()
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        if let foodType = FoodType(rawValue: indexPath.row) {
+        
+            let foodTypeText = FoodType.stringForType(foodType)
+            
+            let chef = AbstractChefFactory.chefForFoodType(foodType)
+            let foodName = chef.cook().name
+            
+            cell.textLabel?.text = "\(foodTypeText): \(foodName)"
+        }
+        
+        return cell
+    }
+
+}
