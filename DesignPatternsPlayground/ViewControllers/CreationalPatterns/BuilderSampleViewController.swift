2fea216ae9e956602256b18744e564bd43ea6e2e
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2015-12-30 18:51:17 +0700

    Move model-related logic from BuilderSampleViewController to BuilderSampleViewModel

--- DesignPatternsPlayground/ViewControllers/CreationalPatterns/BuilderSampleViewController.swift
+++ DesignPatternsPlayground/ViewControllers/CreationalPatterns/BuilderSampleViewController.swift
@@ -8,29 +8,6 @@
 
 import UIKit
 
-
-enum BurgerOrderRows: Int {
-    case Burger = 0
-    case Drinks = 1
-    case Fries = 2
-    case Salads = 3
-    case Sweets = 4
-    
-    static func numberOfRows() -> Int {
-        return 5
-    }
-    
-    func toString() -> String {
-        switch self {
-        case .Burger: return "Burger"
-        case .Drinks: return "Drinks"
-        case .Fries: return "Fries"
-        case .Salads: return "Salads"
-        case .Sweets: return "Sweets"
-        }
-    }
-}
-
 class BuilderSampleViewController: UITableViewController {
         
     @IBOutlet private var headerView: UIView!
@@ -38,10 +15,7 @@ class BuilderSampleViewController: UITableViewController {
     
     private let reuseIdentifier = "reuseIdentifier"
     
-    private var selectionMarker = Array(
-        count: BurgerOrderRows.numberOfRows(),
-        repeatedValue: false
-    )
+    private let viewModel = BuilderSampleViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,7 +37,7 @@ class BuilderSampleViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return BurgerOrderRows.numberOfRows()
+        return viewModel.orderPartNames.count
     }
 
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -71,20 +45,14 @@ class BuilderSampleViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath)
 
         cell.selectionStyle = .None
-        cell.textLabel?.text = ""
-        
-        if let row = BurgerOrderRows(rawValue: indexPath.row) {
-            cell.textLabel?.text = row.toString()
-        }
-        
-        let cellSelected = (indexPath.row < selectionMarker.count) &&
-            (selectionMarker[indexPath.row] == true)
+        cell.textLabel?.text = viewModel.orderPartNames[indexPath.row]
 
+        let cellSelected = viewModel.orderPartSelectedMarker[indexPath.row]
         cell.accessoryType = cellSelected ? .Checkmark : .None
-        
 
         return cell
     }
@@ -100,10 +68,7 @@ class BuilderSampleViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
-        if indexPath.row < selectionMarker.count {
-            selectionMarker[indexPath.row] = true
-
-            tableView.reloadRowsAtIndexPaths([ indexPath ], withRowAnimation: UITableViewRowAnimation.Fade)
-        }
+        viewModel.orderPartSelectedMarker[indexPath.row] = true
+        tableView.reloadRowsAtIndexPaths([ indexPath ], withRowAnimation: .Fade)
     }
 }
---
+++ DesignPatternsPlayground/ViewModels/BuilderSampleViewModel.swift
@@ -0,0 +1,73 @@
+//
+//  BuilderSampleViewModel.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 12/30/15.
+//  Copyright © 2015 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+enum BurgerOrderRows: Int {
+    case Burger = 0
+    case Drinks = 1
+    case Fries = 2
+    case Salads = 3
+    case Sweets = 4
+    
+    static func numberOfRows() -> Int {
+        return 5
+    }
+    
+    func toString() -> String {
+        switch self {
+        case .Burger: return "Burger"
+        case .Drinks: return "Drinks"
+        case .Fries: return "Fries"
+        case .Salads: return "Salads"
+        case .Sweets: return "Sweets"
+        }
+    }
+}
+
+enum BurgerOrderPackage: String {
+    case Normal = "Normal Package"
+    case Whopper = "WHOPPER™ Package"
+}
+
+class BuilderSampleViewModel {
+    
+    // MARK: Properties
+    
+    internal private(set) var orderPartNames: [ String ] = []
+    
+    internal var orderPartSelectedMarker = Array(
+        count: BurgerOrderRows.numberOfRows(),
+        repeatedValue: false
+    )
+    
+    internal private(set) var orderPackages = [
+        BurgerOrderPackage.Normal.rawValue,
+        BurgerOrderPackage.Whopper.rawValue
+    ]
+    
+    internal var selectedPackage = BurgerOrderPackage.Normal
+    
+    // MARK: Public methods
+    
+    init() {
+        prepareOrderPartNames()
+    }
+    
+    // MARK: Private methods
+    
+    private func prepareOrderPartNames() {
+
+        for var index = 0; index < BurgerOrderRows.numberOfRows(); index += 1 {
+            
+            if let row = BurgerOrderRows(rawValue: index) {
+                orderPartNames.append(row.toString())
+            }
+        }
+    }
+}
