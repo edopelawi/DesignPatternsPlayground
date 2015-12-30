426ffd4944db9f36daee28a17b8eb59215d04876
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2015-12-30 17:31:52 +0700

    Initial commit for BuilderSampleViewController

---
+++ DesignPatternsPlayground/ViewControllers/CreationalPatterns/BuilderSampleViewController.swift
@@ -0,0 +1,74 @@
+//
+//  BuilderSampleViewController.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 12/30/15.
+//  Copyright © 2015 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+
+enum BurgerOrderRows: Int {
+    case Burger = 0
+    case Drinks = 1
+    case Fries = 2
+    case Salads = 3
+    case Sweets = 4
+    
+    static func numberOfRows() -> Int {
+        return 5
+    }
+    
+    func toString() -> String {
+        switch self {
+        case .Burger: return "Burger"
+        case .Drinks: return "Drinks"
+        case .Fries: return "Fries"
+        case .Salads: return "Salads"
+        case .Sweets: return "Sweets"
+        }
+    }
+}
+
+class BuilderSampleViewController: UITableViewController {
+        
+    @IBOutlet private var headerView: UIView!
+    @IBOutlet private var footerView: UIView!
+    
+    private let reuseIdentifier = "reuseIdentifier"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        
+        // TODO: Add headerView and footerView here.
+    }
+    
+    // TODO: Add headerView and footerView's action here.
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return BurgerOrderRows.numberOfRows()
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath)
+
+        cell.selectionStyle = .Blue
+        cell.textLabel?.text = ""
+        
+        if let row = BurgerOrderRows(rawValue: indexPath.row) {
+            cell.textLabel?.text = row.toString()
+        }
+
+        return cell
+    }
+    
+}
