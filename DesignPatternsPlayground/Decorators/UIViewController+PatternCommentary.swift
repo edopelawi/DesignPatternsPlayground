197035f235dd1137ac88c5727299bb3f2d2c7c96
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-11 16:16:14 +0700

    Add UIViewController+PatternCommentary

---
+++ DesignPatternsPlayground/Decorators/UIViewController+PatternCommentary.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewController+PatternCommentary.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/11/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    /// Push commentary page for passed `creationalPatternType`. Will only work if this instance has `navigationController`.
+    func pushCommentaryPage(creationalPatternType creationalPatternType: CreationalPatternType) {
+        
+        let commentaries = PatternCommentaryFactory.commentaryForCreationalPattern(creationalPatternType)
+        
+        let commentPage = CommentaryViewController()
+        commentPage.setCommentaryText(commentaries)
+        
+        self.navigationController?.pushViewController(commentPage, animated: true)
+    }
+    
+}
