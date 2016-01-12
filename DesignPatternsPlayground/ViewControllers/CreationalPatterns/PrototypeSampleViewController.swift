e8b115bdc58beafff48a558fdc07722b3a4bb4e6
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-12 14:23:08 +0700

    Initial commit for PrototypeSampleViewController

---
+++ DesignPatternsPlayground/ViewControllers/CreationalPatterns/PrototypeSampleViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PrototypeSampleViewController.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/12/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+class PrototypeSampleViewController: UITableViewController {
+
+    @IBOutlet var headerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableHeaderView = headerView
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+}
