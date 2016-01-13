729c51e72b8de0c6c01ba8e05a1b31e2da37d49e
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-13 09:58:55 +0700

    Initial commit for Diary model

---
+++ DesignPatternsPlayground/Models/Diary.swift
@@ -0,0 +1,38 @@
+//
+//  Diary.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/13/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+class Diary {
+    
+    static private var privateSingletonDiary: Diary = {
+
+        var newDiary = Diary(
+            name: "My Diary",
+            diaryText: "Dear diary, why do I like dairy?"
+        )
+        
+        print("privateSingletonDiary is created!")
+        
+        return newDiary
+    }()
+    
+    /// Returns shared `Diary` instance that used throughout the app.
+    static internal var sharedDiary: Diary {
+        return privateSingletonDiary
+    }
+    
+    internal var name: String
+    internal var diaryText: String
+    
+    /// Private initializer, because Singleton!
+    private init(name: String, diaryText: String) {
+        self.name = name
+        self.diaryText = diaryText
+    }
+}
