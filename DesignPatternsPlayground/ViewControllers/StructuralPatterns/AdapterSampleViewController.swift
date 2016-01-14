565a06026dc8826d9d40eb721a587f89863a09c6
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-14 16:10:56 +0700

    Initial commit on AdapterSampleViewController

---
+++ DesignPatternsPlayground/ViewControllers/StructuralPatterns/AdapterSampleViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AdapterSampleViewController.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/14/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+class AdapterSampleViewController: UIViewController {
+
+    @IBOutlet private weak var placeButton: UIButton!
+    @IBOutlet private weak var orderButton: UIButton!
+
+    @IBOutlet private weak var generatedDialogueLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
