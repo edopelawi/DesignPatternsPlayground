d211c1f8ea898884271a6379ae5d2550f9878acd
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-19 10:06:38 +0700

    Initial commit for AnimalCulinaryTourDialogue struct

---
+++ DesignPatternsPlayground/Models/AnimalCulinaryTourDialogue.swift
@@ -0,0 +1,33 @@
+//
+//  AnimalCulinaryTourDialogue.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/19/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+
+struct AnimalCulinaryTourDialogue {
+    
+    // MARK: Names
+    internal var touristName = ""
+    internal var tourGuideName = ""
+    internal var tourChefName = ""
+    
+    // MARK: Dialogues
+    
+    internal var touristDialogue = ""
+    internal var tourGuideDialogue = ""
+    internal var tourChefDialogue = ""
+    
+    /// Returns `String` representation of full dialogue from this instance's properties.
+    internal func createFullDialogueString -> String {
+        var fullDialogue = "\(touristName): \(touristDialogue)\n"
+        fullDialogue += "\(tourGuideName): \(tourGuideDialogue)\n"
+        fullDialogue += "\(tourChefName): \(tourChefDialogue)"
+        
+        return fullDialogue
+    }
+}
