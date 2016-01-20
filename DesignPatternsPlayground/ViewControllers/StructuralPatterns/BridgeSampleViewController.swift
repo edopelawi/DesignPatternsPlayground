e1a51f7c50392d275feda0dde31e425ef763d283
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-20 14:49:15 +0700

    Initial commit for BridgeSampleViewController

---
+++ DesignPatternsPlayground/ViewControllers/StructuralPatterns/BridgeSampleViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BridgeSampleViewController.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/20/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+class BridgeSampleViewController: UIViewController {
+
+    @IBOutlet weak var terrainSelectionButton: UIButton!
+    
+    @IBOutlet weak var terrainTopLabel: UILabel!
+    @IBOutlet weak var terrainBottomLabel: UILabel!
+    
+    @IBOutlet weak var jamesBondCarLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
