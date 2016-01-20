adbe1277e44591fcde7c011fae5dfad82a816594
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-20 15:32:03 +0700

    Make BridgeSampleViewController uses corresponding viewModel

--- DesignPatternsPlayground/ViewControllers/StructuralPatterns/BridgeSampleViewController.swift
+++ DesignPatternsPlayground/ViewControllers/StructuralPatterns/BridgeSampleViewController.swift
@@ -7,18 +7,77 @@
 //
 
 import UIKit
+import JamesBondTools
 
-class BridgeSampleViewController: UIViewController {
+class BridgeSampleViewController: UIViewController, BridgeSampleViewModelDelegate {
 
-    @IBOutlet weak var terrainSelectionButton: UIButton!
+    @IBOutlet private weak var terrainSelectionButton: UIButton!
     
-    @IBOutlet weak var terrainTopLabel: UILabel!
-    @IBOutlet weak var terrainBottomLabel: UILabel!
+    @IBOutlet private weak var terrainTopLabel: UILabel!
+    @IBOutlet private weak var terrainBottomLabel: UILabel!
     
-    @IBOutlet weak var jamesBondCarLabel: UILabel!
+    @IBOutlet private weak var jamesBondCarLabel: UILabel!
+    
+    private var viewModel = BridgeSampleViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel.delegate = self
+        loadStringsFromCurrentViewModel()
+    }
+    
+    // MARK: IBOutlets
+    
+    @IBAction func showTerrainOptions(sender: UIButton) {
+        
+        let actionSheet = UIAlertController(title: "Terrain Types", message: "", preferredStyle: .ActionSheet)
+        
+        for terrain in viewModel.terrainOptions {
+            
+            let action = UIAlertAction(
+                title: terrain.rawValue,
+                style: .Default,
+                handler: {
+                    [weak self] _ in
+                    self?.viewModel.selectedTerrain = terrain
+                }
+            )
+            
+            actionSheet.addAction(action)
+        }
+        
+        self.presentViewController(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+    
+    // MARK: BridgeSampleViewModelDelegate methods
+    
+    func selectedTerrainUpdated(viewModel: BridgeSampleViewModel, selectedTerrain: JamesBondTerrainType) {
+        let selectionButtonTitle = selectedTerrain.rawValue
+        terrainSelectionButton.setTitle(selectionButtonTitle, forState: .Normal)
+        terrainSelectionButton.setTitle(selectionButtonTitle, forState: .Highlighted)
+    }
+    
+    func bondCarWithEffectsUpdated(viewModel: BridgeSampleViewModel, bondCarWithEffects: String) {
+        jamesBondCarLabel.text = bondCarWithEffects
+    }
+    
+    func terrainEmojisUpdated(viewModel: BridgeSampleViewModel, terrainEmojis: String) {
+        terrainTopLabel.text = terrainEmojis
+        terrainBottomLabel.text = terrainEmojis
+    }
+    
+    // MARK: Private methods
+    
+    private func loadStringsFromCurrentViewModel() {
+        terrainTopLabel.text = viewModel.terrainEmojis
+        terrainBottomLabel.text = viewModel.terrainEmojis
+        jamesBondCarLabel.text = viewModel.bondCarWithEffects
+        
+        let selectionButtonTitle = viewModel.selectedTerrain.rawValue
+        terrainSelectionButton.setTitle(selectionButtonTitle, forState: .Normal)
+        terrainSelectionButton.setTitle(selectionButtonTitle, forState: .Highlighted)
     }
 
 }
