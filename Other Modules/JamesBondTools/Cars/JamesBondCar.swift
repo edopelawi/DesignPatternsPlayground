d416e753c2127897d2e4431d6ba676ec143ea172
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-20 12:02:59 +0700

    Add JamesBondCar to JamesBondTools

---
+++ Other Modules/JamesBondTools/Cars/JamesBondCar.swift
@@ -0,0 +1,26 @@
+//
+//  JamesBondCar.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/20/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+public enum JamesBondTerrainType: String {
+    case Air = "Air"
+    case Road = "Road"
+    case Mountain = "Mountain"
+    case Sea = "Sea"
+}
+
+public class JamesBondCar {
+    
+    
+    init(terrainType: JamesBondTerrainType) {
+        // TODO: choose the internal implementations here later.
+    }
+    
+    
+}
