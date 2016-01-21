305206bec49987a92750d727405aa1e774c2c33c
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-21 14:19:20 +0700

    Implement TreasureHUntRoomCollectionViewCell in TreasureHuntAlleyRoomViewController

--- DesignPatternsPlayground/ViewControllers/StructuralPatterns/TreasureHunts/TreasureHuntAlleyRoomViewController.swift
+++ DesignPatternsPlayground/ViewControllers/StructuralPatterns/TreasureHunts/TreasureHuntAlleyRoomViewController.swift
@@ -10,25 +10,71 @@ import UIKit
 
 class TreasureHuntAlleyRoomViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    private var alleyRoom: TreasureHuntAlleyRoom?
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     @IBOutlet var headerView: UICollectionReusableView!
     @IBOutlet weak var headerViewLabel: UILabel!
     
+    init(alleyRoom: TreasureHuntAlleyRoom) {
+        super.init(nibName: "TreasureHuntAlleyRoomViewController", bundle: nil)
+        self.alleyRoom = alleyRoom
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        registerCells()
+        registerHeaderView()
+    }
+    
+    // MARK: Private methods
+    
+    private func registerCells() {
+        let cellNib = TreasureHuntRoomCollectionViewCell.nib()
+        let cellIdentifier = TreasureHuntRoomCollectionViewCell.identifier
+        
+        collectionView.registerNib(cellNib, forCellWithReuseIdentifier: cellIdentifier)
+    }
+    
+    private func registerHeaderView() {
+        // TODO: Implement this method later.
     }
     
     // MARK: UICollectionView delegate and datasource
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // TODO: Update this later.
-        return 0
+        
+        if let validRoom = alleyRoom {
+            return validRoom.numberOfNextRooms()
+        } else {
+            return 0
+        }
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         // TODO: Update this later.
-        return UICollectionViewCell()
+        
+        let identifier = TreasureHuntRoomCollectionViewCell.identifier
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath) as! TreasureHuntRoomCollectionViewCell
+        
+        let index = indexPath.row
+        
+        if let nextRoom = alleyRoom?.retrieveTreasureHuntRoomAtIndex(index) {
+            cell.configureForTreasureHuntRoom(nextRoom)
+        }
+        
+        return cell
     }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        // TODO: Push corresponding page later.
+    }
+    
 
 }
