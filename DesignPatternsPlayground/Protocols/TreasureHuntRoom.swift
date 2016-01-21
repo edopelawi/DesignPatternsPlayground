56737c5674a655133f594353107aeafd0ce6e46c
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-21 09:51:46 +0700

    Initial commit for TreasureHuntRoom protocol

---
+++ DesignPatternsPlayground/Protocols/TreasureHuntRoom.swift
@@ -0,0 +1,24 @@
+//
+//  TreasureHuntRoom.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/21/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+enum TreasureHuntRoomContentType {
+    case MoreRooms
+    case Treasure
+    case Monster
+}
+
+protocol TreasureHuntRoom {
+    
+    /// Returns content type of this instance.
+    func roomContentType() -> TreasureHuntRoomContentType
+    
+    /// Returns `String` representation of current content.
+    func roomContent() -> String
+}
