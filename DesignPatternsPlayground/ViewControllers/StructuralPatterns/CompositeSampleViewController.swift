035ba6e72d09ae4851f65e3fe6d42189e9dcd322
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-21 17:48:46 +0700

    Initial commit for CompositeSampleViewController

---
+++ DesignPatternsPlayground/ViewControllers/StructuralPatterns/CompositeSampleViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CompositeSampleViewController.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/21/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import UIKit
+
+class CompositeSampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Dungeon of Treasures!"
+        navigationController?.navigationBarHidden = false
+    }
+
+    @IBAction func enterEasyDungeon(sender: UIButton) {
+        let depth = 3
+        pushDungeonWithDepth(depth)
+    }
+
+    @IBAction func enterMediumDungeon(sender: UIButton) {
+        let depth = 7
+        pushDungeonWithDepth(depth)
+    }
+    
+    @IBAction func enterHardDungeon(sender: UIButton) {
+        let depth = 12
+        pushDungeonWithDepth(depth)
+    }
+    
+    // MARK: Private methods
+    
+    private func pushDungeonWithDepth(depth: Int) {
+        let alleyRoom = TreasureHuntRoomFactory.treasureHuntRoomsWithDepth(depth) as! TreasureHuntAlleyRoom
+        let viewController = TreasureHuntAlleyRoomViewController(alleyRoom: alleyRoom)
+        
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
