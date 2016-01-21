abf4ce80ab53d0c9412f2544796aebd28a9f8d97
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-21 10:45:16 +0700

    Initial commit for TreasureHuntMonsterRoom and TreasureHuntTreasureRoom

---
+++ DesignPatternsPlayground/Models/TreasureHuntRooms/TreasureHuntMonsterRoom.swift
@@ -0,0 +1,31 @@
+//
+//  TreasureHuntMonsterRoom.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/21/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+class TreasureHuntMonsterRoom: TreasureHuntRoom {
+    
+    private let possibleMonsters = [
+        "👻", "👹", "👺",
+        "💀", "👽", "👾",
+        "🐲"
+    ]
+    
+    func roomContentType() -> TreasureHuntRoomContentType {
+        return .Monster
+    }
+    
+    /// Returns emoji representation of current monster in the room.
+    func roomContent() -> String {
+        if let monster = possibleMonsters.randomElement() {
+            return monster
+        } else {
+            return ""
+        }
+    }
+}
---
+++ DesignPatternsPlayground/Models/TreasureHuntRooms/TreasureHuntTreasureRoom.swift
@@ -0,0 +1,21 @@
+//
+//  TreasureHuntTreasureRoom.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/21/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+class TreasureHuntTreasureRoom: TreasureHuntRoom {
+    
+    func roomContentType() -> TreasureHuntRoomContentType {
+        return .Treasure
+    }
+    
+    /// Returns emoji presentation of the treasure that stored in this room.
+    func roomContent() -> String {
+        return "👑"
+    }
+}
