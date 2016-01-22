368e0f7356b0fcecac6d5e371ee9684f92b77a30
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-22 11:23:50 +0700

    Initial commit for FireFightingArmament

---
+++ DesignPatternsPlayground/Decorators/FightingMoves/FireFightingArmament.swift
@@ -0,0 +1,26 @@
+//
+//  FireFightingArmament.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/22/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+class FireFightingArmament: FightingMoves {
+    
+    private var fighter: FightingMoves
+    
+    init(fighter: FightingMoves) {
+        self.fighter = fighter
+    }
+    
+    func punch() -> String {
+        return "🔥\(fighter.punch())🔥"
+    }
+    
+    func kick() -> String {
+        return "🔥\(fighter.kick())🔥"
+    }
+}
