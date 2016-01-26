6254c01c9fdd2f72c34c2e53b6e24a8d084fb75d
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-26 13:30:35 +0700

    Add ThemeParkVisitor model

---
+++ DesignPatternsPlayground/Models/ThemeParkVisitor.swift
@@ -0,0 +1,14 @@
+//
+//  ThemeParkVisitor.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/26/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+struct ThemeParkVisitor {
+    /// Represents height for a theme park visitor.
+    internal var height = 0
+}
