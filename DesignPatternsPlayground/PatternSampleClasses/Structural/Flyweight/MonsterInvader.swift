1e4020a1427349fff60608525927df64a9b132c6
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-28 18:38:00 +0700

    Initial commit for MonsterInvader model

---
+++ DesignPatternsPlayground/PatternSampleClasses/Structural/Flyweight/MonsterInvader.swift
@@ -0,0 +1,15 @@
+//
+//  MonsterInvader.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/28/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+class MonsterInvader {
+    
+    /// Emoji representation of this monster invader.
+    internal var icon = ""
+}
