6f3f25c980ddeac7427b87325bb61e8a6d8f9239
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-01-28 18:51:04 +0700

    Initial commit for MonsterInvaderFlyweightFactory

---
+++ DesignPatternsPlayground/PatternSampleClasses/Structural/Flyweight/MonsterInvaderFlyweightFactory.swift
@@ -0,0 +1,29 @@
+//
+//  MonsterInvaderFlyweightFactory.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 1/28/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+class MonsterInvaderFlyweightFactory {
+    
+    private var invaderHashMap = [String: MonsterInvader]()
+    
+    /// Returns `MonsterInvader` instance with passed `icon`.
+    internal func retrieveInvaderWithIcon(icon: String) -> MonsterInvader {
+        
+        if let invader = invaderHashMap[icon] {
+            return invader
+        }
+        
+        let newInvader = MonsterInvader()
+        newInvader.icon = icon
+        
+        invaderHashMap[icon] = newInvader
+        
+        return newInvader
+    }
+}
