d050c9f0ddd6e997467cfc39de2eed6ff513a8e1
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-03-23 16:28:46 +0700

    Use PatternTypeTableController in BehaviouralPatternsViewController

--- DesignPatternsPlayground/PatternSampleClasses/Behavioural/BehaviouralPatternsViewController.swift
+++ DesignPatternsPlayground/PatternSampleClasses/Behavioural/BehaviouralPatternsViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class BehaviouralPatternsViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private var tableController: PatternTypeTableController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         navigationController?.navigationBarHidden = false
         title = "Behavioural Design Patterns"
+        
+        tableView.backgroundColor = view.backgroundColor
+        
+        tableController = PatternTypeTableController(
+            tableView: tableView,
+            patternGroupType: .Behavioural
+        )
+        
+        tableController.patternTypeSelectedClosure = {
+            [weak self] patternType in
+            
+            print("Selected pattern: \(patternType)")
+            
+            if let creationalType = patternType as? CreationalPatternType {
+                self?.pushPageForPatternType(creationalType)
+            }
+        }
     }
 
+    // MARK: - Private Methods -
+    
+    private func pushPageForPatternType(patternType: CreationalPatternType) {
+        
+        var viewController: UIViewController?
+        
+        // TODO: Push appropriate view controllers here
+        switch patternType {
+        default: break
+        }
+        
+        if let viewController = viewController {
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
 }
