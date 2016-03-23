d9b7895b2a66c49bf2961fb8e2646c00d2e90444
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-03-23 10:56:27 +0700

    Use ChristmasLights' preferredVoltage for ChristmasDecoration's baseVoltage

--- DesignPatternsPlayground/PatternSampleClasses/Structural/Facade/ChristmasDecoration.swift
+++ DesignPatternsPlayground/PatternSampleClasses/Structural/Facade/ChristmasDecoration.swift
@@ -12,7 +12,7 @@ import Foundation
 
 class ChristmasDecoration: DCRechargeableBatteryDelegate {
   
-    private var baseVoltage = 0.5
+    private var baseVoltage = ChristmasLights.preferredVoltage
     
     internal var battery: DCRechargeableBattery!
     internal var christmasLights = [ChristmasLights]()
