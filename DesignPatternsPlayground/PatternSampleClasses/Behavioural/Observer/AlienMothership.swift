a58b0120e2ecb1588eee3ee2d61e01d0adda3dda
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-03-30 09:31:19 +0700

    Initial commit for AlienMothership class

---
+++ DesignPatternsPlayground/PatternSampleClasses/Behavioural/Observer/AlienMothership.swift
@@ -0,0 +1,13 @@
+//
+//  AlienMothership.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 3/30/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+class AlienMothership {
+    // TODO: Add properties here later.
+}
