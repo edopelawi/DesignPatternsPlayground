43d11b3585fa74f7c1aa04df88f70ce30293a058
Ricardo Pramana Suranta <ricardo@icehousecorp.com>
2016-03-31 12:53:52 +0700

    Initial commit for FelineDailyActivity enum

---
+++ DesignPatternsPlayground/PatternSampleClasses/Behavioural/TemplateMethod/FelineDailyActivity.swift
@@ -0,0 +1,18 @@
+//
+//  FelineDailyActivity.swift
+//  DesignPatternsPlayground
+//
+//  Created by Ricardo Pramana Suranta on 3/31/16.
+//  Copyright © 2016 Ricardo Pramana Suranta. All rights reserved.
+//
+
+import Foundation
+
+enum FelineDailyActivity {
+    case Sleep
+    case Bath
+    case Eat
+    case Hunt
+    case Play
+    case Bored
+}
